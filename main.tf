702806cd6226ee3c6ae00feaa44ac7ec552d994e
muvvalaakash <113330264+muvvaalaakash@users.noreply.github.com>
2026-05-21 22:24:16 +0530

    Update azurerm provider version and configuration

--- main.tf
+++ main.tf
@@ -1,20 +1,19 @@
+# ==============================================================
+# TERRAFORM + PROVIDER
+# ==============================================================
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "=3.80.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "YOUR_SUBSCRIPTION_ID"
+  skip_provider_registration = true
 }
-
 # =========================================
 # RESOURCE GROUP
 # =========================================
