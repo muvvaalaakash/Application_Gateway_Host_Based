6b77d76a79615629291b1acecfeb917562041a50
muvvalaakash <113330264+muvvaalaakash@users.noreply.github.com>
2026-05-21 22:16:27 +0530

    Add Terraform variables for configuration

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "Central India"
+}
+
+variable "resource_group_name" {
+  default = "RG-HostBasedRouting"
+}
+
+variable "admin_username" {
+  default = "Akash"
+}
+
+variable "admin_password" {
+  default = "Akash@21042004"
+}
